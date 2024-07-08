c63bd6ff28299ba99f715ad202d45eae62c1fa0d
Becca <becca.starr@kayenta.io>
2024-07-08 23:33:36 +0100

    count_Lights method and complete part one

--- christmas_lights.py
+++ christmas_lights.py
@@ -45,12 +45,46 @@ class ChristmasLights:
 
         return self
 
+    def count_lights(self, state: State) -> int:
+        count = 0
+
+        for light in self.grid:
+            if light.state == state:
+                count += 1
+
+        return count
+
     @property
     def grid_info(self) -> dict[str, int]:
-        return {"x": self.x, "y": self.y, "total_lights": self.x * self.y}
+        return {
+            "x": self.x,
+            "y": self.y,
+            "total_lights": self.x * self.y,
+            "lights_on": self.count_lights(State.ON),
+            "lights_off": self.count_lights(State.OFF),
+        }
 
 
 if __name__ == "__main__":
+    # Initialize the Christmas Lights
     lights = ChristmasLights(1000, 1000)
+    start = lights.grid_info
+    print(f"START: {start}")
 
-    print(lights.grid_info)
+    # Santa's Instructions
+    lights.turn_on(x=(887, 9), y=(959, 629))
+    lights.turn_on(x=(454, 398), y=(844, 448))
+    lights.turn_off(x=(539, 243), y=(559, 965))
+    lights.turn_off(x=(370, 819), y=(676, 868))
+    lights.turn_off(x=(145, 40), y=(370, 997))
+    lights.turn_off(x=(301, 3), y=(808, 453))
+    lights.turn_on(x=(351, 678), y=(951, 908))
+    lights.toggle(x=(720, 196), y=(897, 994))
+    lights.toggle(x=(831, 394), y=(904, 860))
+
+    # Get the final state of the Christmas Lights
+    end = lights.grid_info
+    print(f"END: {end}")
+
+    # After following Santa's instructions, how many lights are on?
+    print(f"Lights On: {end['lights_on']}")
