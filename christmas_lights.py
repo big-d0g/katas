19fac9a27fd3458b7fa6d834cd9fa6fea63db7e7
Becca <becca.starr@kayenta.io>
2024-07-08 23:11:20 +0100

    add turn_off method for lights

--- christmas_lights.py
+++ christmas_lights.py
@@ -1,16 +1,36 @@
 from enum import Enum
+from dataclasses import dataclass
+from typing import Self
 
 class State(Enum):
     ON = 1
     OFF = 0
+
+@dataclass
+class Light:
+    x: int
+    y: int
+    state: State
 
 
 class ChristmasLights:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
-        self.grid = self.init_grid
+        self.grid = self.init_grid()
 
-    def init_grid(self) -> list[tuple[int, int, State]]:
-        return [(x, y, State.OFF) for x in range(self.x) for y in range(self.y)]
+    def init_grid(self) -> list[Light]:
+        return [Light(x, y, State.OFF) for x in range(self.x) for y in range(self.y)]
     
+    def turn_on(self, x: tuple[int, int], y: tuple[int, int]) -> Self:
+        for light in self.grid:
+            if x[0] >= light.x >= x[1] and y[0] >= light.y >= y[1]:
+                light.state = State.ON
+        
+        return self
+        
+
+lights = ChristmasLights(10, 10)
+lights.turn_on((0, 0), (5, 5))
+
+print(lights.grid)
